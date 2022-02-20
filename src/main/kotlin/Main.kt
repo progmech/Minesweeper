a411cf4e79816d806077f4716881a42e587b4356
Andrei Astakhov <andrei.astakhov@hotmail.com>
2022-02-20 19:40:16 +0300

    Stage 3/5: Look around you

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,11 @@ package minesweeper
 import kotlin.random.Random
 
 const val FIELD_SIZE = 9
+const val LOWER_BOUND = 0
+const val UPPER_BOUND = FIELD_SIZE - 1
 
 fun main() {
-    val fieldRange = 0 until FIELD_SIZE
+
     val field = arrayOf(
         CharArray(FIELD_SIZE) {'.'},
         CharArray(FIELD_SIZE) {'.'},
@@ -19,20 +21,121 @@ fun main() {
     )
 
     println("How many mines do you want on the field?")
-    var mineCount = readLine()!!.toInt()
+    val mineCount = readLine()!!.toInt()
+    field.setMines(mineCount)
+    field.fill()
+    field.print(FIELD_SIZE)
+}
 
-    while(mineCount != 0) {
+fun Array<CharArray>.setMines(mineCount: Int) {
+    var count = mineCount
+    while(count != 0) {
         val newX = Random.nextInt(0, FIELD_SIZE)
         val newY = Random.nextInt(0, FIELD_SIZE)
-        if (field[newY][newX] == 'X') continue
-        field[newY][newX] = 'X'
-        mineCount--
+        if (this[newY][newX] == 'X') continue
+        this[newY][newX] = 'X'
+        count--
     }
+}
 
+fun Array<CharArray>.fill() {
+    for (y in 0 until FIELD_SIZE) {
+        for (x in 0 until FIELD_SIZE) {
+            if (this[y][x] == 'X') continue
+            if (isTopLeftCorner(x, y)) {
+                this[y][x] = countMines(charArrayOf(this[y + 1][x], this[y][x + 1], this[y + 1][x + 1]))
+                continue
+            }
+            if (isTopRightCorner(x, y)) {
+                this[y][x] = countMines(charArrayOf(this[y][x - 1], this[y + 1][x], this[y + 1][x - 1]))
+                continue
+            }
+            if (isBottomLeftCorner(x, y)) {
+                this[y][x] = countMines(charArrayOf(this[y-1][x], this[y][x+1], this[y - 1][x + 1]))
+                continue
+            }
+            if (isBottomRightCorner(x, y)) {
+                this[y][x] = countMines(charArrayOf(this[y-1][x], this[y][x-1], this[y - 1][x - 1]))
+                continue
+            }
+            if (isTopSide(x, y)) {
+                this[y][x] = countMines(charArrayOf(
+                    this[y][x-1],
+                    this[y+1][x-1],
+                    this[y+1][x],
+                    this[y+1][x+1],
+                    this[y][x + 1]
+                ))
+                continue
+            }
+            if (isBottomSide(x, y)) {
+                this[y][x] = countMines(charArrayOf(
+                    this[y][x-1],
+                    this[y-1][x-1],
+                    this[y-1][x],
+                    this[y-1][x+1],
+                    this[y][x + 1]
+                ))
+                continue
+            }
+            if (isLeftSide(x, y)) {
+                this[y][x] = countMines(charArrayOf(
+                    this[y-1][x],
+                    this[y-1][x+1],
+                    this[y][x+1],
+                    this[y+1][x+1],
+                    this[y+1][x]
+                ))
+                continue
+            }
+            if (isRightSide(x, y)) {
+                this[y][x] = countMines(charArrayOf(
+                    this[y-1][x],
+                    this[y-1][x-1],
+                    this[y][x-1],
+                    this[y+1][x-1],
+                    this[y+1][x]
+                ))
+                continue
+            }
+            this[y][x] = countMines(charArrayOf(
+                this[y-1][x-1],
+                this[y-1][x],
+                this[y-1][x+1],
+                this[y][x+1],
+                this[y+1][x+1],
+                this[y+1][x],
+                this[y+1][x-1],
+                this[y][x-1]
+            ))
+        }
+    }
+}
+
+fun countMines(cells: CharArray): Char {
+    val c = cells.count { it == 'X' }
+    return if (c == 0) {
+        '.'
+    } else {
+        c.digitToChar()
+    }
+}
+
+fun Array<CharArray>.print(fieldSize: Int) {
+    val fieldRange = 0 until fieldSize
     for (y in fieldRange) {
         for (x in fieldRange) {
-            print(field[y][x])
+            print(this[y][x])
         }
         println("")
     }
 }
+
+fun isTopLeftCorner(x: Int, y: Int) = x == LOWER_BOUND && y == LOWER_BOUND
+fun isTopRightCorner(x: Int, y: Int) = x == UPPER_BOUND && y == LOWER_BOUND
+fun isBottomLeftCorner(x: Int, y: Int) = x == LOWER_BOUND && y == UPPER_BOUND
+fun isBottomRightCorner(x: Int, y: Int) = x == UPPER_BOUND && y == UPPER_BOUND
+fun isTopSide(x: Int, y: Int) = x != LOWER_BOUND && x != UPPER_BOUND && y == LOWER_BOUND
+fun isBottomSide(x: Int, y: Int) = x != LOWER_BOUND && x != UPPER_BOUND && y == UPPER_BOUND
+fun isLeftSide(x: Int, y: Int) = x == LOWER_BOUND && y != LOWER_BOUND && y != UPPER_BOUND
+fun isRightSide(x: Int, y: Int) = x == UPPER_BOUND && y != LOWER_BOUND && y != UPPER_BOUND
